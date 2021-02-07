dac14d9db6cf22a09dd9c9fa2bc67d5e0856096e
Daniela <daniela.fedyakin@gmail.com>
2021-02-07 20:55:15 +0100

    add readme and starter-files

---
+++ starter-files/script.js
@@ -0,0 +1,10 @@
+/**
+ *  STEPS:
+ *
+ *  1. Create a class to GET weather data, attach the CORS using .ajaxPrefilter to the request and GET the woid
+ *  2. Use the response from the step above and GET the weather for that location
+ *  3. Deconstruct the data received and shape it to what the UI expects it to look like before sending to the UI
+ *  4. Add error/loading states and cover edge use cases
+ *  5. Divide classes per function to have a more clean code approach and separation on concerns
+ *
+ */
